caa7123d42783fbf78675eda9295cb9ba9ee5d18
James Beardsley <beards18@gmail.com>
2016-02-03 23:26:05 -0500

    awesome comix gui update
    added issue 2, made the issue navigation bar show currently selected
    issue, ran awesomecomix.html through w3c validator

--- js/awesomecomix.js
+++ js/awesomecomix.js
@@ -8,22 +8,30 @@ $(document).ready(function(){
 	
 	//Issue selection
 	$("#issueSelect").on("click", "button", function(e){
+		//Takes the current issue number from the ID of the button element
 		currentIssue = $(this).attr("id").slice(5,7);
 		currentPage = 0;
 		
+		//Title string
 		var output = "<h2>Issue ";
 		if (parseInt(currentIssue) < 10) {
 			output += currentIssue.slice(1)
 		} else {output += currentIssue};
 		output += "</h2>";
 		
+		//Change highlighting
+		$("#issueSelect button").removeClass("btn-primary").addClass("btn-info");
+		$(this).removeClass("btn-info").addClass("btn-primary");
+		
 		$("#issueTitle").html(output);
 		$("#currentPage").html(pageImage());
 		$(".previousPage").attr("disabled", "disabled");
+		$(".nextPage").removeAttr("disabled");
 	});
 	
 	//Page selection
 	$(".pageSelect").on("click", "button", function(e){
+		//Previous page
 		if ($(this).hasClass("previousPage")) {
 			currentPage -= 1;
 			$("#currentPage").html(pageImage());
@@ -31,10 +39,12 @@ $(document).ready(function(){
 				$(".previousPage").attr("disabled", "disabled")
 			};
 			$(".nextPage").removeAttr("disabled");
-		} else if ($(this).hasClass("nextPage")) {
+		} 
+		//Next page
+		else if ($(this).hasClass("nextPage")) {
 			currentPage += 1;
 			$("#currentPage").html(pageImage());
-			if (currentPage === 6) {
+			if (currentPage === 6) { 
 				$(".nextPage").attr("disabled", "disabled")
 			};
 			$(".previousPage").removeAttr("disabled");
